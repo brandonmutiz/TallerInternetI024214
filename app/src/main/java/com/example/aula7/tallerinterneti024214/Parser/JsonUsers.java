9bc506b6c0b1e6d790e2b7613c7030d933a3498c
pc705 <pc705@iucesmag.edu.co>
2018-04-17 13:57:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/aula7/tallerinterneti024214/Parser/JsonUsers.java
@@ -0,0 +1,43 @@
+package com.example.aula7.tallerinterneti024214.Parser;
+
+import com.example.aula7.tallerinterneti024214.Model.Users;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by AULA 7 on 17/04/2018.
+ */
+
+public class JsonUsers {
+
+    public static List<Users> getData(String content) throws JSONException {
+
+        JSONArray jsonArray = new JSONArray(content);
+        List<Users> UsersList = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++){
+
+            JSONObject item = jsonArray.getJSONObject(i);
+
+            Users users = new Users();
+            users.setCodigo(item.getString("codigo"));
+            users.setNombre(item.getString("nombre"));
+            users.setEdad(item.getString("edad"));
+            users.setCorreo(item.getString("correo"));
+            users.setPass(item.getString("pass"));
+
+            UsersList.add(users);
+
+        }
+
+        return UsersList;
+    }
+
+}
+
+}
